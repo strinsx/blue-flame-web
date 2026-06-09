a31adfa13009eb449aa2f823562558b5d23031f2
astral.strinsx <157059279+strinsx@users.noreply.github.com>
2026-06-09 16:50:22 +0800

    feat: movie detail interface

--- app/types/movie.ts
+++ app/types/movie.ts
@@ -20,5 +20,28 @@ export interface Carousel {
     popularity: number,
     backdrop_path: string,
     media_type: string,
-    
+
+}
+
+export interface MovieDetails {
+    id: number,
+    title: string,
+    overview: string,
+    tagline: string,
+    popularity: number,
+    vote_average: number,
+    vote_count: number,
+    runtime: number,
+    status: string,
+    media_type: string,
+    poster_path: string,
+    backdrop_path: string,
+    release_date: string,
+    revenue: number,
+    budget: number,
+    original_language: string,
+    genres: { id: number, name: string }[],
+    production_companies: { id: number, name: string, logo_path: string, origin_country: string }[],
+    production_countries: { iso_3166_1: string, name: string }[],
+    belongs_to_collection: { id: number, name: string, poster_path: string, backdrop_path: string } | null,
 }
