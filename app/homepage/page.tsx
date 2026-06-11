d75b14b3cab837a0494144a46e014a4ffd00708d
astral.strinsx <157059279+strinsx@users.noreply.github.com>
2026-06-11 10:43:52 +0800

    refactor: media queries + navigation

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -2,13 +2,13 @@ import { CardPanel } from "../components/movie/Cardpanel";
 import { MoviePanel } from "../components/movie/MoviePanel";
 import { Navbar } from "../components/Navbar";
 import { TrendingPanel } from "../components/movie/TrendingPanel";
-import {Footer} from '../components/Footer'
+import { Footer } from '../components/Footer'
 import { TopRatedPanel } from "../components/movie/TopRatedPanel";
 import { OnAirTv } from "../components/series/OnAir";
 import { TrendingPanelTV } from "../components/series/TrendingPanelTV";
 export default function Homepage() {
   return (
-    <div className="flex flex-col w-[70%] mx-auto min-h-screen">
+    <div className="flex flex-col w-full md:w-[85%] lg:w-[70%] mx-auto min-h-screen">
       <Navbar />
       <CardPanel />
       <MoviePanel />
