ab8007e30efbccbb1d6eee9f0009635536007298
astral.strinsx <157059279+strinsx@users.noreply.github.com>
2026-06-12 15:42:04 +0800

    fix: query params

--- app/api/movies/similar/route.ts
+++ app/api/movies/similar/route.ts
@@ -1,17 +1,19 @@
 
 import { getSimilar } from "@/app/lib/tmdb/movie"
 
-export async function GET(){
+export async function GET(request: Request) {
     try {
 
-        return Response.json(await getSimilar())
-        
+        const { searchParams } = new URL(request.url)
+        const movieId = Number(searchParams.get('id'))
+        return Response.json(await getSimilar(movieId))
+
     } catch (error) {
 
         return Response.json(
-            {error: 'Failed to fetch'}, 
-            {status: 500}
+            { error: 'Failed to fetch' },
+            { status: 500 }
         )
-        
+
     }
 }
